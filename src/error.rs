182d8d8f82d2a15d451bed1d9214580d2daac5cd
Kikuo Emoto <kemoto@codemonger.io>
2023-09-13 18:01:07 +0900

    feat(error): coerce AWS SDK and serde errors
    - Coerces the following errors into the common error:
        - `SdkError`
        - `ByteStreamError`
        - `serde_json::Error`

--- src/error.rs
+++ src/error.rs
@@ -4,6 +4,8 @@
 #[derive(Debug)]
 pub enum Error {
     InvalidData(String),
+    SerdeJsonError(serde_json::Error),
+    AwsSdkError(String),
 }
 
 impl std::error::Error for Error {}
@@ -12,6 +14,26 @@ impl std::fmt::Display for Error {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
             Error::InvalidData(s) => write!(f, "Invalid data: {}", s),
+            Error::SerdeJsonError(e) => write!(f, "serde_json::Error: {}", e),
+            Error::AwsSdkError(s) => write!(f, "AWS SDK error: {}", s),
         }
+    }
+}
+
+impl From<serde_json::Error> for Error {
+    fn from(e: serde_json::Error) -> Self {
+        Error::SerdeJsonError(e)
+    }
+}
+
+impl<E, R> From<aws_sdk_s3::error::SdkError<E, R>> for Error {
+    fn from(e: aws_sdk_s3::error::SdkError<E, R>) -> Self {
+        Error::AwsSdkError(format!("{}", e))
+    }
+}
+
+impl From<aws_sdk_s3::primitives::ByteStreamError> for Error {
+    fn from(e: aws_sdk_s3::primitives::ByteStreamError) -> Self {
+        Error::AwsSdkError(format!("{}", e))
     }
 }
