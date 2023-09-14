c9367b83eff9fe1c193dad9693cf9eb84bfb07bc
Kikuo Emoto <kemoto@codemonger.io>
2023-09-15 08:04:35 +0900

    feat: introduce clap
    - Introduces `clap` in `main` to separate commands. There is only one
      command `create` for now.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use anyhow::{Context, Error, anyhow};
+use clap::{Parser, Subcommand};
 use std::env;
 use std::fs::{File, create_dir_all};
 use std::path::Path;
@@ -8,19 +9,43 @@ use url::Url;
 use mumble_embedding::posts::{create_embeddings_for_posts, list_posts};
 use mumble_embedding::streams::StreamAsyncExt;
 
+#[derive(Parser)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// Creates embeddings for user's posts.
+    Create {
+        /// Username whose posts are to be processed.
+        username: String,
+        /// Output directory where embedding results are to be saved.
+        out_dir: String,
+    }
+}
+
 #[tokio::main]
 async fn main() -> Result<(), Error> {
-    let args: Vec<String> = env::args().collect();
-    let username = args.get(1).ok_or(anyhow!("no username specified"))?;
-    let out_dir = args.get(2).ok_or(anyhow!("no output directory specified"))?;
+    let cli = Cli::parse();
+    match cli.command {
+        Commands::Create { username, out_dir } => {
+            create(username, out_dir).await?;
+        }
+    }
+    Ok(())
+}
+
+async fn create(username: String, out_dir: String) -> Result<(), Error> {
     let objects_bucket_name = env::var("OBJECTS_BUCKET_NAME")
         .context("no OBJECTS_BUCKET_NAME set")?;
     println!("objects bucket name: {}", objects_bucket_name);
     let openai_api_key = env::var("OPENAI_API_KEY")
         .context("no OPENAI_API_KEY set")?;
     println!("output directory: {}", out_dir);
-    if !Path::new(out_dir).exists() {
-        create_dir_all(out_dir)?;
+    if !Path::new(&out_dir).exists() {
+        create_dir_all(&out_dir)?;
     }
     println!("pulling mumblings of {}", username);
     let posts = list_posts(&objects_bucket_name, &username).await;
@@ -45,7 +70,7 @@ async fn main() -> Result<(), Error> {
                     .ok_or(anyhow!("invalid ID: {}", embedding.id))?
                     .last()
                     .ok_or(anyhow!("invalid ID: {}", embedding.id))?;
-                let path = Path::new(out_dir).join(name).with_extension("json");
+                let path = Path::new(&out_dir).join(name).with_extension("json");
                 println!("saving embedding to {:?}", path);
                 let out = File::create(path)?;
                 serde_json::to_writer(out, &embedding)?;
