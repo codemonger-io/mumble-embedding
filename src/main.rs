d4d0bfe12af8817f6dee7b8f5d086c9c8917d6ef
Kikuo Emoto <kemoto@codemonger.io>
2023-09-15 15:56:45 +0900

    feat: build flechasdb from embeddings
    - `main` introduces a new command `build` that builds a vector database
      from embedding vectors with flechasdb.

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,22 @@
-use anyhow::{Context, Error, anyhow};
+use anyhow::{Context, Error, anyhow, bail};
 use clap::{Parser, Subcommand};
 use std::env;
-use std::fs::{File, create_dir_all};
+use std::fs::{File, create_dir_all, read_dir};
 use std::path::Path;
 use tokio_stream::StreamExt;
 use url::Url;
 
-use mumble_embedding::posts::{create_embeddings_for_posts, list_posts};
+use flechasdb::db::{DatabaseBuilder, DatabaseBuilderEvent, DatabaseQueryEvent};
+use flechasdb::db::proto::serialize_database;
+use flechasdb::io::LocalFileSystem;
+use flechasdb::vector::BlockVectorSet;
+
+use mumble_embedding::openai::{EmbeddingRequestBody, create_embeddings};
+use mumble_embedding::posts::{
+    Embedding,
+    create_embeddings_for_posts,
+    list_posts,
+};
 use mumble_embedding::streams::StreamAsyncExt;
 
 #[derive(Parser)]
@@ -23,7 +33,16 @@ enum Commands {
         username: String,
         /// Output directory where embedding results are to be saved.
         out_dir: String,
-    }
+    },
+    /// Builds a vector database from embedding results.
+    Build {
+        /// Input directory where embedding results are to be loaded from.
+        in_dir: String,
+        /// Output directory where the vector database are saved.
+        out_dir: String,
+        /// Test query.
+        test_query: Option<String>,
+    },
 }
 
 #[tokio::main]
@@ -32,7 +51,10 @@ async fn main() -> Result<(), Error> {
     match cli.command {
         Commands::Create { username, out_dir } => {
             create(username, out_dir).await?;
-        }
+        },
+        Commands::Build { in_dir, out_dir, test_query } => {
+            build(in_dir, out_dir, test_query).await?;
+        },
     }
     Ok(())
 }
@@ -80,5 +102,142 @@ async fn create(username: String, out_dir: String) -> Result<(), Error> {
             }
         };
     }
+    Ok(())
+}
+
+async fn build(
+    in_dir: String,
+    out_dir: String,
+    test_query: Option<String>,
+) -> Result<(), Error> {
+    const RESERVED_VECTORS: usize = 1000;
+    const VECTOR_SIZE: usize = 1536; // OpenAI embedding vector
+    const NUM_PARTITIONS: usize = 1;
+    const NUM_DIVISIONS: usize = 12;
+    const NUM_CODES: usize = 10;
+    let mut data: Vec<f32> = Vec::with_capacity(RESERVED_VECTORS * VECTOR_SIZE);
+    let mut contents: Vec<String> = Vec::with_capacity(RESERVED_VECTORS);
+    for entry in read_dir(in_dir)? {
+        let entry = entry?;
+        println!("loading: {:?}", entry.file_name());
+        let file = File::open(entry.path())?;
+        let embedding: Embedding = serde_json::from_reader(file)?;
+        if embedding.embedding.len() != VECTOR_SIZE {
+            bail!("invalid vector size: {}", embedding.embedding.len());
+        }
+        data.extend(embedding.embedding.iter().map(|v| *v as f32));
+        contents.push(embedding.content);
+    }
+    let vs = BlockVectorSet::chunk(data, VECTOR_SIZE.try_into()?)?;
+    let time = std::time::Instant::now();
+    let mut event_time = std::time::Instant::now();
+    let db = DatabaseBuilder::new(vs)
+        .with_partitions(NUM_PARTITIONS.try_into().unwrap())
+        .with_divisions(NUM_DIVISIONS.try_into().unwrap())
+        .with_clusters(NUM_CODES.try_into().unwrap())
+        .build(Some(move |event| {
+            match event {
+                DatabaseBuilderEvent::StartingIdAssignment |
+                DatabaseBuilderEvent::StartingPartitioning |
+                DatabaseBuilderEvent::StartingSubvectorDivision |
+                DatabaseBuilderEvent::StartingQuantization(_) => {
+                    event_time = std::time::Instant::now();
+                },
+                DatabaseBuilderEvent::FinishedIdAssignment => {
+                    println!(
+                        "assigned vector IDs in {} μs",
+                        event_time.elapsed().as_micros(),
+                    );
+                },
+                DatabaseBuilderEvent::FinishedPartitioning => {
+                    println!(
+                        "partitioned data in {} μs",
+                        event_time.elapsed().as_micros(),
+                    );
+                },
+                DatabaseBuilderEvent::FinishedSubvectorDivision => {
+                    println!(
+                        "divided data in {} μs",
+                        event_time.elapsed().as_micros(),
+                    );
+                },
+                DatabaseBuilderEvent::FinishedQuantization(i) => {
+                    println!(
+                        "quantized division {} in {} μs",
+                        i,
+                        event_time.elapsed().as_micros(),
+                    );
+                },
+            };
+        }))?;
+    println!("built database in {} μs", time.elapsed().as_micros());
+
+    println!("saving database to {}", out_dir);
+    let mut fs = LocalFileSystem::new(&out_dir);
+    serialize_database(&db, &mut fs)?;
+
+    // makes a test query if one is given
+    if let Some(test_query) = test_query {
+        const K: usize = 10; // k-nearest neighbors
+        const NPROBE: usize = 1;
+        let openai_api_key = env::var("OPENAI_API_KEY")
+            .context("no OPENAI_API_KEY set")?;
+        let query_embedding = create_embeddings(
+            &EmbeddingRequestBody {
+                model: "text-embedding-ada-002".to_string(),
+                input: vec![test_query.to_string()],
+                user: Some("mumble_embedding".to_string()),
+            },
+            openai_api_key,
+        ).await?;
+        let query_vector: Vec<f32> = query_embedding.data[0].embedding
+            .iter()
+            .map(|x| *x as f32)
+            .collect();
+        let mut event_time = std::time::Instant::now();
+        let results = db.query(
+            &query_vector,
+            K.try_into()?,
+            NPROBE.try_into()?,
+            Some(move |event| {
+                match event {
+                    DatabaseQueryEvent::StartingPartitionSelection |
+                    DatabaseQueryEvent::StartingPartitionQuery(_) |
+                    DatabaseQueryEvent::StartingResultSelection => {
+                        event_time = std::time::Instant::now();
+                    },
+                    DatabaseQueryEvent::FinishedPartitionSelection => {
+                        println!(
+                            "selected partitions in {} μs",
+                            event_time.elapsed().as_micros(),
+                        );
+                    },
+                    DatabaseQueryEvent::FinishedPartitionQuery(i) => {
+                        println!(
+                            "queried partition {} in {} μs",
+                            i,
+                            event_time.elapsed().as_micros(),
+                        );
+                    },
+                    DatabaseQueryEvent::FinishedResultSelection => {
+                        println!(
+                            "selected results in {} μs",
+                            event_time.elapsed().as_micros(),
+                        );
+                    },
+                }
+            }),
+        )?;
+        println!("testing query: {}", test_query);
+        for (i, result) in results.iter().enumerate() {
+            println!(
+                "result[{}]:\ncontent: {}\napprox. distance: {}",
+                i,
+                contents[result.vector_index],
+                result.squared_distance,
+            );
+        }
+    }
+
     Ok(())
 }
