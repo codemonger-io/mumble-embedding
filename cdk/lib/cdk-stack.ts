02fc798343c56b1ea930ab6d978c210b800e819a
Kikuo Emoto <kemoto@codemonger.io>
2023-09-19 23:49:27 +0900

    feat(cdk): provision query function
    - `CdkStack` provisions the `query` Lambda function, and the S3 bucket
      to store database files.

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -1,16 +1,42 @@
-import * as cdk from 'aws-cdk-lib';
+import * as path from 'node:path';
+import {
+  CfnOutput,
+  Duration,
+  RemovalPolicy,
+  Stack,
+  StackProps,
+  aws_lambda as lambda,
+  aws_s3 as s3,
+} from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
-export class CdkStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+import { RustFunction } from 'cargo-lambda-cdk';
+
+export class CdkStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const databaseBucket = new s3.Bucket(this, 'DatabaseBucket', {
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+      encryption: s3.BucketEncryption.S3_MANAGED,
+      removalPolicy: RemovalPolicy.RETAIN,
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const queryLambda = new RustFunction(this, 'QueryLambda', {
+      architecture: lambda.Architecture.ARM_64,
+      manifestPath: path.join('lambda', 'database', 'Cargo.toml'),
+      binaryName: 'query',
+      environment: {
+        DATABASE_BUCKET_NAME: databaseBucket.bucketName,
+      },
+      memorySize: 256,
+      timeout: Duration.seconds(30),
+    });
+    databaseBucket.grantRead(queryLambda);
+
+    new CfnOutput(this, 'DatabaseBucketName', {
+      description: 'Name of the S3 bucket for database',
+      value: databaseBucket.bucketName,
+    });
   }
 }
