d0ee6953277a8d8be1ce46c54f7430a56ff3deb0
Kikuo Emoto <kemoto@codemonger.io>
2023-10-09 17:10:57 +0900

    feat(lib): split post into sentences
    - `posts` introduces a new function `split_post_into_sentences` that
      splits a given post into sentences. Each sentence is represented by a
      new struct `PostSentence`. Also replaces `create_embeddings_for_posts`
      with `create_embeddings_for_sentences`.

--- lib/src/posts.rs
+++ lib/src/posts.rs
@@ -1,11 +1,14 @@
 //! Dealing with posts (mumblings).
 
+use core::ops::Range;
 use futures::stream::{Stream, StreamExt};
 use serde::{Deserialize, Serialize};
 
+use crate::markdown::extract_text_blocks;
 use crate::error::Error;
 use crate::openai::{EmbeddingRequestBody, create_embeddings};
 use crate::s3::ObjectList;
+use crate::text::extract_sentences;
 
 /// Post.
 #[derive(Clone, Debug, Deserialize)]
@@ -72,6 +75,43 @@ async fn load_post(
     Ok(post)
 }
 
+/// Sentence in a post.
+#[derive(Clone, Debug)]
+pub struct PostSentence {
+    /// ID of the source post.
+    pub post_id: String,
+    /// Content.
+    pub content: String,
+    /// Range in the post.
+    pub range: Range<usize>,
+}
+
+impl PostSentence {
+    /// Returns the ID of the sentence.
+    pub fn id(&self) -> String {
+        format!("{}#{}-{}", self.post_id, self.range.start, self.range.end)
+    }
+}
+
+/// Splits a post into sentences.
+pub fn split_post_into_sentences(post: Post) -> Vec<PostSentence> {
+    let content = if let Some(source) = post.source {
+        source.content
+    } else {
+        post.content
+    };
+    extract_text_blocks(&content)
+        .unwrap()
+        .into_iter()
+        .flat_map(|block| extract_sentences(&block))
+        .map(|(sentence, range)| PostSentence {
+            post_id: post.id.clone(),
+            content: sentence,
+            range,
+        })
+        .collect()
+}
+
 /// Embedding of a content.
 #[derive(Clone, Debug, Deserialize, Serialize)]
 pub struct Embedding {
@@ -83,38 +123,30 @@ pub struct Embedding {
     pub embedding: Vec<f64>,
 }
 
-/// Creates embeddings for given posts.
-pub async fn create_embeddings_for_posts(
-    posts: Vec<Post>,
+/// Creates embeddings for given sentences.
+pub async fn create_embeddings_for_sentences(
+    sentences: Vec<PostSentence>,
     api_key: String,
 ) -> Result<Vec<Embedding>, Error> {
     let request = EmbeddingRequestBody {
         model: format!("text-embedding-ada-002"),
-        input: posts.iter()
-            .map(|p| {
-                if let Some(source) = p.source.as_ref() {
-                    source.content.clone()
-                } else {
-                    p.content.clone()
-                }
-            })
-            .collect(),
+        input: sentences.iter().map(|s| s.content.clone()).collect(),
         user: Some(format!("mumble_embedding")),
     };
     let res = create_embeddings(&request, api_key).await?;
     println!("usage: {:?}", res.usage);
     let mut data = res.data;
-    if posts.len() != data.len() {
+    if sentences.len() != data.len() {
         return Err(Error::InvalidData(
             format!("failed to create embeddings of one or more posts"),
         ));
     }
     data.sort_by_key(|d| d.index);
-    let embeddings = posts.into_iter()
+    let embeddings = sentences.into_iter()
         .zip(request.input.into_iter())
         .zip(data.into_iter())
-        .map(|((p, content), d)| Embedding {
-            id: p.id,
+        .map(|((s, content), d)| Embedding {
+            id: s.id(),
             content,
             embedding: d.embedding,
         })
