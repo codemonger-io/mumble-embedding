74c7de82d08cb84c6d624cfaeb675b15efd61607
Kikuo Emoto <kemoto@codemonger.io>
2023-10-09 15:01:22 +0900

    fix(lib): update transducer state by token string
    - Fixes the issue that a necessary whitespace was dropped before an
      inline code or URL. Updates the transducer state when an inline code
      or URL is observed.
    
      Now the transducer trims leading whitespaces.

--- lib/src/text.rs
+++ lib/src/text.rs
@@ -22,9 +22,24 @@ pub fn extract_sentences(
 fn extract_sentences_from_fragments(
     fragments: &Vec<Fragment>,
 ) -> Vec<(String, Range<usize>)> {
-    let senetences: Vec<(String, Range<usize>)> = fragments
+    let (mut tokens, state): (Vec<Token>, TransducerState) = fragments
         .iter()
-        .flat_map(|fragment| segment_fragment(fragment))
+        .fold(
+            (Vec::with_capacity(10), TransducerState::Initial),
+            |(mut tokens, state), fragment| {
+                let (new_tokens, state) =
+                    segment_fragment(state, fragment);
+                tokens.extend(new_tokens);
+                (tokens, state)
+            },
+        );
+    let mut transducer = Transducer::new_from(
+        state,
+        fragments.last().map(|(_, r)| r.end).unwrap_or(0),
+    );
+    tokens.extend(transducer.finish());
+    let senetences: Vec<(String, Range<usize>)> = tokens
+        .into_iter()
         .fold(Vec::with_capacity(10), |mut sentences, (token, r)| {
             match token {
                 TokenType::Character(ch) => {
@@ -55,13 +70,14 @@ fn extract_sentences_from_fragments(
         .collect()
 }
 
-fn segment_fragment((content, range): &Fragment) -> Vec<Token> {
+fn segment_fragment(
+    state: TransducerState,
+    (content, range): &Fragment,
+) -> (Vec<Token>, TransducerState) {
     match content {
-        FragmentContent::Text(text) => segment_text(text, range),
-        FragmentContent::Code(code) =>
-            vec![(TokenType::String(code.clone()), range.clone())],
-        FragmentContent::Url(url) =>
-            vec![(TokenType::String(url.clone()), range.clone())],
+        FragmentContent::Text(text) => segment_text(state, text, range),
+        FragmentContent::Code(code) => pass_token_string(state, code, range),
+        FragmentContent::Url(url) => pass_token_string(state, url, range),
     }
 }
 
@@ -69,15 +85,32 @@ fn segment_fragment((content, range): &Fragment) -> Vec<Token> {
 //
 // A sentence breaks at a period, question mark, exclamation mark,
 // semicolon, or 句点('。').
-fn segment_text(text: &String, range: &Range<usize>) -> Vec<Token> {
+fn segment_text(
+    state: TransducerState,
+    text: &String,
+    range: &Range<usize>,
+) -> (Vec<Token>, TransducerState) {
     // labels each character
-    let mut transducer = Transducer::new(range.start);
+    let mut transducer = Transducer::new_from(state, range.start);
     let mut tokens: Vec<Token> = Vec::with_capacity(text.len());
     for ch in text.chars() {
-        tokens.append(&mut transducer.next(ch));
+        tokens.extend(transducer.next(ch));
     }
-    tokens.append(&mut transducer.finish());
-    tokens
+    (tokens, transducer.state.unwrap())
+}
+
+// Passes a given token string through a transducer.
+//
+// A token string is not split into sentences, but the transducer state may
+// transition.
+fn pass_token_string(
+    state: TransducerState,
+    text: &String,
+    range: &Range<usize>,
+) -> (Vec<Token>, TransducerState) {
+    let mut transducer = Transducer::new_from(state, range.start);
+    let tokens = transducer.next_string(text);
+    (tokens, transducer.state.unwrap())
 }
 
 struct Transducer {
@@ -117,16 +150,24 @@ enum TransducerState {
 }
 
 impl Transducer {
-    fn new(start: usize) -> Self {
+    fn new_from(state: TransducerState, start: usize) -> Self {
         Self {
             num_chars: start,
-            state: Some(TransducerState::Initial),
+            state: Some(state),
         }
     }
 
     fn next(&mut self, ch: char) -> Vec<Token> {
         let (next_state, output) = self.state.take().unwrap().next(self, ch);
         self.num_chars += 1;
+        self.state.replace(next_state);
+        output
+    }
+
+    fn next_string(&mut self, text: &String) -> Vec<Token> {
+        let (next_state, output) =
+            self.state.take().unwrap().next_string(self, text);
+        self.num_chars += text.len();
         self.state.replace(next_state);
         output
     }
@@ -157,6 +198,28 @@ impl TransducerState {
         }
     }
 
+    fn next_string(
+        self,
+        transducer: &mut Transducer,
+        text: &String,
+    ) -> (Self, Vec<Token>) {
+        match self {
+            Self::Initial => Self::initial_next_string(transducer, text),
+            Self::Character => Self::character_next_string(transducer, text),
+            Self::Whitespace(start) =>
+                Self::whitespace_next_string(transducer, start, text),
+            Self::PeriodAnd(start) =>
+                Self::period_and_next_string(transducer, start, text),
+            Self::WhitespacePeriodAnd(w_start, p_start) =>
+                Self::whitespace_period_and_next_string(
+                    transducer,
+                    w_start,
+                    p_start,
+                    text,
+                ),
+        }
+    }
+
     fn finish(self, transducer: &mut Transducer) -> (Self, Vec<Token>) {
         match self {
             Self::Initial => Self::initial_finish(),
@@ -175,8 +238,8 @@ impl TransducerState {
     ) -> (Self, Vec<Token>) {
         match ch {
             ch if ch.is_ascii_whitespace() => {
-                // deters the output and squashes the consecutive whitespaces
-                (Self::Whitespace(transducer.num_chars), Vec::new())
+                // ignores the leading whitespaces
+                (Self::Initial, Vec::new())
             },
             _ => {
                 (
@@ -191,6 +254,22 @@ impl TransducerState {
                 )
             },
         }
+    }
+
+    fn initial_next_string(
+        transducer: &mut Transducer,
+        text: &String,
+    ) -> (Self, Vec<Token>) {
+        (
+            Self::Character,
+            vec![(
+                TokenType::String(text.clone()),
+                Range {
+                    start: transducer.num_chars,
+                    end: transducer.num_chars + text.chars().count(),
+                },
+            )],
+        )
     }
 
     fn initial_finish() -> (Self, Vec<Token>) {
@@ -248,6 +327,22 @@ impl TransducerState {
         }
     }
 
+    fn character_next_string(
+        transducer: &mut Transducer,
+        text: &String,
+    ) -> (Self, Vec<Token>) {
+        (
+            Self::Character,
+            vec![(
+                TokenType::String(text.clone()),
+                Range {
+                    start: transducer.num_chars,
+                    end: transducer.num_chars + text.chars().count(),
+                },
+            )],
+        )
+    }
+
     fn character_finish() -> (Self, Vec<Token>) {
         (Self::Initial, Vec::new())
     }
@@ -296,6 +391,32 @@ impl TransducerState {
         }
     }
 
+    fn whitespace_next_string(
+        transducer: &mut Transducer,
+        start: usize,
+        text: &String,
+    ) -> (Self, Vec<Token>) {
+        (
+            Self::Character,
+            vec![
+                (
+                    TokenType::Character(' '),
+                    Range {
+                        start,
+                        end: transducer.num_chars,
+                    },
+                ),
+                (
+                    TokenType::String(text.clone()),
+                    Range {
+                        start: transducer.num_chars,
+                        end: transducer.num_chars + text.chars().count(),
+                    },
+                ),
+            ],
+        )
+    }
+
     fn whitespace_finish(
         transducer: &Transducer,
         start: usize,
@@ -320,8 +441,9 @@ impl TransducerState {
         match ch {
             ch if ch.is_ascii_whitespace() => {
                 // determines the end of the sentence
+                // and ignores subsequent whitespaces
                 (
-                    Self::Whitespace(transducer.num_chars),
+                    Self::Initial,
                     vec![
                         (
                             TokenType::Character('.'),
@@ -365,6 +487,33 @@ impl TransducerState {
         }
     }
 
+    fn period_and_next_string(
+        transducer: &mut Transducer,
+        start: usize,
+        text: &String,
+    ) -> (Self, Vec<Token>) {
+        // cancels the end of the sentence
+        (
+            Self::Character,
+            vec![
+                (
+                    TokenType::Character('.'),
+                    Range {
+                        start,
+                        end: start + 1,
+                    },
+                ),
+                (
+                    TokenType::String(text.clone()),
+                    Range {
+                        start: transducer.num_chars,
+                        end: transducer.num_chars + text.chars().count(),
+                    },
+                ),
+            ],
+        )
+    }
+
     fn period_and_finish(start: usize) -> (Self, Vec<Token>) {
         (
             Self::Initial,
@@ -397,6 +546,7 @@ impl TransducerState {
             ch if ch.is_ascii_whitespace() => {
                 // determines the end of the sentence
                 // drops the preceding whitespace
+                // and ignores the subsequent whitespaces
                 (
                     Self::Initial,
                     vec![
@@ -448,6 +598,42 @@ impl TransducerState {
                 )
             },
         }
+    }
+
+    fn whitespace_period_and_next_string(
+        transducer: &Transducer,
+        w_start: usize,
+        p_start: usize,
+        text: &String,
+    ) -> (Self, Vec<Token>) {
+        // cancels the end of the sentence
+        // leaves the preceding whitespace
+        (
+            Self::Character,
+            vec![
+                (
+                    TokenType::Character(' '),
+                    Range {
+                        start: w_start,
+                        end: p_start,
+                    },
+                ),
+                (
+                    TokenType::Character('.'),
+                    Range {
+                        start: p_start,
+                        end: p_start + 1,
+                    },
+                ),
+                (
+                    TokenType::String(text.clone()),
+                    Range {
+                        start: transducer.num_chars,
+                        end: transducer.num_chars + text.chars().count(),
+                    },
+                ),
+            ],
+        )
     }
 
     fn whitespace_period_and_finish(p_start: usize) -> (Self, Vec<Token>) {
